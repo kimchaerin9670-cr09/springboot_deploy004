174b470762f1268650269ea9f99d13570409d31f
rin9009 <kimchaerin999@gmail.com>
2026-06-04 09:50:12 +0900

    feet: 새로운 프로젝트 생성

---
+++ deploy004_springboot/src/main/java/com/min/edu/ctrl/HomeController.java
@@ -0,0 +1,14 @@
+package com.min.edu.ctrl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String getMethodName() {
+		return new String("Spring Boot 프로젝트");
+	}
+	
+}
